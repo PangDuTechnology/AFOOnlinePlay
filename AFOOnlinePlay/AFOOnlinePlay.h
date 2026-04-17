8d0c7b5f3d269bfa43f3ca723ee3691cf28c31b9
PangDu <792229197@qq.com>
2026-04-17 16:17:33 +0800

    Initial Commit

---
+++ AFOOnlinePlay/AFOOnlinePlay.h
@@ -0,0 +1,12 @@
+//
+//  AFOOnlinePlay.h
+//  AFOOnlinePlay
+//
+//  Created by zhaoyun on 2026/4/17.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AFOOnlinePlay : NSObject
+
+@end
---
+++ AFOOnlinePlay/AFOOnlinePlay.m
@@ -0,0 +1,12 @@
+//
+//  AFOOnlinePlay.m
+//  AFOOnlinePlay
+//
+//  Created by zhaoyun on 2026/4/17.
+//
+
+#import "AFOOnlinePlay.h"
+
+@implementation AFOOnlinePlay
+
+@end
